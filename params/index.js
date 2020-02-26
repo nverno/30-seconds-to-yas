f4f86713d1f7395a96161398c8a0bf7a73e3cd33
nverno <noah.v.peart@gmail.com>
2020-02-25 22:01:02 -0500

    squash

--- params/index.js
+++ params/index.js
@@ -2,6 +2,7 @@ const DEFAULT_CONFIG = {
   prefix: "f_",
   overwriteSnippets: true,
   jsSnippetUri: 'https://raw.githubusercontent.com/30-seconds/30-seconds-of-code/master/vscode_snippets/snippets.json',
+  directory: "30s_snippets"
 };
 
 module.exports = {
